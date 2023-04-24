7131cab09a8aa59359b055724c761b79a4978b4b
MarcosH09 <marcos.hernandezl@alumnos.upm.es>
2023-04-24 21:33:27 +0200

    Interfaz
    1

---
+++ INterfaz.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+	
+	//INTERFAZ
+	printf("*************************************************\n"); 
+	printf("*                                               *\n");
+	printf("*                                               *\n"); 
+	printf("*           OBJETIVO MEDIO AMBIENTE             *\n");
+	printf("*           -----------------------             *\n"); 
+	printf("*          Investigacion de las aguas           *\n");
+	printf("*                  madrileñas                   *\n"); 
+	printf("*                                               *\n");
+	printf("*                                               *\n"); 
+	printf("*                                               *\n");
+	printf("*************************************************\n"); 
+	
+	printf("Bienvenidos a nuestro proyecto de investigacion acerca de\n"); 
+	printf("las aguas madrilenas, donde podras conocer informacion\n ");
+	printf("interactuando con cada una de las opciones que hay debajo\n"); 
+	printf("1."); 
+	printf("2.");
+	printf("3.");
+	printf("4.");
+	printf("5.");
+	printf("6.");
+	printf("7.");
+	
+}
