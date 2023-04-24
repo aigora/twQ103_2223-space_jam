716829f60476f25fe9a03b5209127cd8356ad6eb
MarcosH09 <marcos.hernandezl@alumnos.upm.es>
2023-04-24 22:47:37 +0200

    Update INterfaz.cpp

--- INterfaz.cpp
+++ INterfaz.cpp
@@ -1,6 +1,10 @@
 #include <stdio.h>
+#include <math.h>
+
 
 int main(){
+	int opcion, numfuente; 
+	
 	
 	//INTERFAZ
 	printf("*************************************************\n"); 
@@ -9,21 +13,27 @@ int main(){
 	printf("*           OBJETIVO MEDIO AMBIENTE             *\n");
 	printf("*           -----------------------             *\n"); 
 	printf("*          Investigacion de las aguas           *\n");
-	printf("*                  madrileñas                   *\n"); 
+	printf("*                  madrilenas                   *\n"); 
 	printf("*                                               *\n");
 	printf("*                                               *\n"); 
 	printf("*                                               *\n");
 	printf("*************************************************\n"); 
 	
 	printf("Bienvenidos a nuestro proyecto de investigacion acerca de\n"); 
-	printf("las aguas madrilenas, donde podras conocer informacion\n ");
+	printf("las aguas madrilenas, donde podras conocer informacion\n");
 	printf("interactuando con cada una de las opciones que hay debajo\n"); 
-	printf("1."); 
-	printf("2.");
-	printf("3.");
-	printf("4.");
-	printf("5.");
-	printf("6.");
-	printf("7.");
+	printf("     \n"); 
+	printf("1.Informacion acerca de las fuentes\n"); //informacion general acerca de una fuente 
+	printf("2.Cual es el pH maximo de todas las fuentes\n");
+	printf("3.\n");
+	printf("4.\n");
+	printf("5.\n");
+	printf("6.\n");
+	printf("7.\n");
+	
+	switch(opciones){
+		case 1: 
+
+	}
 	
 }
